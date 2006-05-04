df51cfda7ee716dbc26e49e185a8952658ea4d36
Nick Burch <cgi-wiki@gagravarr.org>
2006-05-04 16:04:22 +0000

    Create a parent for Formatters that work on Wiki Links

--- lib/Wiki/Toolkit/Formatter/Default.pm
+++ lib/Wiki/Toolkit/Formatter/Default.pm
@@ -2,13 +2,17 @@ package Wiki::Toolkit::Formatter::Default;
 
 use strict;
 
-use vars qw( $VERSION @_links_found );
+use vars qw( $VERSION @ISA @_links_found );
 $VERSION = '0.02';
+
+use Wiki::Toolkit::Formatter::WikiLinkFormatterParent;
 
 use CGI ":standard";
 use Carp qw(croak carp);
 use Text::WikiFormat as => 'wikiformat';
 use HTML::PullParser;
+
+@ISA = qw( Wiki::Toolkit::Formatter::WikiLinkFormatterParent );
 
 =head1 NAME
 
@@ -135,78 +139,11 @@ sub format {
 			implicit_links => $self->{_implicit_links} } );
 }
 
-=item B<rename_links>
-
-  $formatter->rename_links( $from, $to, $content );
-
-Renames all the links to a certain page in the supplied content.
-(Obviously this is dependent on object properties such as
-C<extended_links> etc.)
-
-=cut
-
-sub rename_links {
-    my ($self, $from, $to, $content) = @_;
-
-	# If we support extended (square bracket) links, update those
-	if($self->{_extended_links}) {
-		$content =~ s/\[$from\]/\[$to\]/g;
-		$content =~ s/\[$from(\s*|.*?)\]/\[$to$1\]/g;
-	}
-
-	# If we support implicit (camelcase) links, update those
-	if($self->{_implicit_links}) {
-		$content =~ s/\b$from\b/$to/g;
-		$content =~ s/^$from\b/$to/gm;
-		$content =~ s/\b$from$/$to/gm;
-	}
-
-	return $content;
-}
-
-=item B<find_internal_links>
-
-  my @links_to = $formatter->find_internal_links( $content );
-
-Returns a list of all nodes that the supplied content links to.
-(Obviously this is dependent on object properties such as
-C<extended_links> etc.)
-
-=cut
-
-sub find_internal_links {
-    my ($self, $raw) = @_;
-
-    @_links_found = ();
-
-    my $foo = wikiformat($raw,
-			{ link => sub {
-					my ($link, $opts) = @_;
-					$opts ||= {};
-					my $title;
-					($link, $title) = split(/\|/, $link, 2)
-						if $opts->{extended};
-					push @Wiki::Toolkit::Formatter::Default::_links_found,
-						$link;
-					return ""; # don't care about output
-				}
-			},
-			{
-				extended       => $self->{_extended_links},
-				prefix         => $self->{_node_prefix},
-				implicit_links => $self->{_implicit_links} 
-			} 
-	);
-
-    my @links = @_links_found;
-    @_links_found = ();
-    return @links;
-}
-
 =back
 
 =head1 SEE ALSO
 
+L<Wiki::Toolkit::Formatter::WikiLinkFormatterParent>
 L<Wiki::Toolkit>
 
 =head1 AUTHOR
